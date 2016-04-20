de1d2e313e827b7b974e677b02866dc37903b03f
Aaron Hickman <aaronhickman311@gmail.com>
2016-04-20 09:58:27 -0600

    changing max query days

--- src/availability/queryDays.js
+++ src/availability/queryDays.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export default Joi.number()
   .min(1)
-  .max(730)
+  .max(370)
   .description('Number of days to query, starting with today')
   .example(180)
   .default(30);
