de41924dd16a6ecae21ec785a87f61f02f1da6fc
Colten Rouska <crouska@gmail.com>
2016-08-18 12:42:33 -0600

    Adding currencyCode

--- test/common.test.js
+++ test/common.test.js
@@ -1,5 +1,6 @@
 import common from '../src/common';
 import { expect } from 'chai';
+import joi from 'joi';
 
 describe('Common Schemas', () => {
 
@@ -36,6 +37,35 @@ describe('Common Schemas', () => {
   describe('bool', () => {
     it('should expose a Joi Object', () => {
       expect(common.bool('description!').isJoi).to.be.true;
+    });
+  });
+
+  describe.only('currencyCode', () =>{
+    it('should expose a joi object', () =>{
+      expect(common.currencyCode.isJoi).to.be.true;
+    });
+
+    it("doesn't allow bogus currencyCode", () =>{
+      expect(() =>{
+        joi.attempt('bye', common.currencyCode);
+      }).to.throw(/must be one of/);
+    });
+
+    it("doesn't throw an error for valid codes", () =>{
+      expect(() =>{
+        joi.attempt('ANG', common.currencyCode);
+      }).to.not.throw();
+    });
+
+    it("doesn't throw for lowercase valid code", () =>{
+      expect(() =>{
+        joi.attempt('xts', common.currencyCode);
+      }).to.not.throw();
+    });
+
+    it('Uppercases codezes', () =>{
+      let value = joi.attempt('zwl', common.currencyCode);
+      expect(value).to.equal('ZWL');
     });
   });
 });
