f2bd8cb78363796348237438a03a69556a72c12c
Jesse Hansen <twindagger@gmail.com>
2016-09-08 11:28:29 -0600

    Add uploadFields

--- src/media/mediaUpload.js
+++ src/media/mediaUpload.js
@@ -1,5 +1,14 @@
 import Joi from 'joi';
 
+let uploadFields = description => Joi.object({
+  title: Joi.string(),
+  description: Joi.string(),
+  categories: Joi.array(),
+  isDefault: Joi.bool().default(false),
+  order: Joi.number().min(0).default(0),
+  file: Joi.any()
+}).description(description);
+
 let mutableFields = description => Joi.object({
   url: Joi.string(),
   title: Joi.string(),
@@ -13,6 +22,10 @@ let mutableFieldsStrict = description => mutableFields(description).requiredKeys
   'url'
 );
 
+let uploadFieldsStrict = description => uploadFields(description).requiredKeys(
+  'file'
+);
+
 let multipleMutableFieldsStrict = description => Joi.array().items(mutableFieldsStrict(description));
 
-export { mutableFields, mutableFieldsStrict, multipleMutableFieldsStrict };
+export { mutableFields, mutableFieldsStrict, multipleMutableFieldsStrict, uploadFields, uploadFieldsStrict };
